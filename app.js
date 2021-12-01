d6b116aa0a1a13acf298b50b62170a315bed3ea0
Carlos Blecha <carlosantony07@outlook.com>
2021-11-30 21:39:02 -0300

    ATT REC_SENHA

--- app.js
+++ app.js
@@ -15,6 +15,31 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(bodyParser.json())
 
+/*var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'carlos123',
+    database: 'usuarios'
+});
+
+connection.connect(function (err){
+    if (err){
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+})*/
+
+/*connection.query('SELECT * FROM users',function(err,rows,fields){
+    if (!err){
+        console.log('Resultado: ', rows);
+    }else{
+        console.log('Erro ao realizar a consulta.');
+    }
+})*/
+
+
+
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.static(path.join(__dirname,"scripts")));
 app.use(express.static(path.join(__dirname,"img")));
@@ -40,9 +65,14 @@ app.get("/logado", function(req,res){
     res.sendFile(__dirname+"/Cast4All/home_logado.html")
 })
 
+app.get("/perfil_logado", function(req,res){
+    res.sendFile(__dirname+"/Cast4All/perfil.html")
+})
+
 //ENVIO DE EMAILS
 
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
+const { type } = require('os');
 
 let transporter = nodemailer.createTransport({
     host: "smtp.office365.com",
@@ -56,26 +86,29 @@ let transporter = nodemailer.createTransport({
 
 
 
-/*app.post("/send-email", function(req,res){
+app.post("/send-email", function(req,res){
     const user = req.body.email
-    Usuarios.findAll({
-        attributes: ['senha'],
-        where: {email: user},
-      })
-      .then((usuarios){
-        let senha = usuarios
-        let senhaT = (senha['senha'])
-        res.send("res: " + senhaT)
-      });
-      
-      
-    /*Usuarios.findAll({where: {email: user}}).then(function(usuarios){
-        if (usuarios == ''){
-            console.log()
-        }else{
-            
-        }
-    })*/
+        Usuarios.findOne({
+            attributes: ['senha'],
+            where: {email: user},
+          })
+          .then(function(usuarios) {
+            senha = usuarios
+            senhaT = senha['senha']
+            transporter.sendMail({
+                from: "Cast 4All <cast4allbr@outlook.com>",
+                to: user,
+                subject: "Olá, sua recuperação de senha do Cast4All chegou!",
+                text: "Se você digitou um email que naõ está cadastrado, sua senha a seguir será nula!, Senha cadastrada: " + senhaT
+            }).then(message => {
+                console.log(message);
+            }).catch(err => {
+                console.log(err);
+            })
+            res.sendFile(__dirname+"/Cast4All/login.html")
+          }).catch(err =>{
+            res.sendFile(__dirname+"/Cast4All/RecuperarSenhaIndex.html")
+          })
 
     /*transporter.sendMail({
         from: "Cast 4All <cast4allbr@outlook.com>",
@@ -87,6 +120,7 @@ let transporter = nodemailer.createTransport({
     }).catch(err => {
         console.log(err);
     })*/
+});
 
 app.post("/auth", function(req,res){
     const user = req.body.email
@@ -115,7 +149,16 @@ app.post('/register', function(req,res){
     })
 })
 
+/*app.post('/auth', function(req,res){
+    const user = req.body.email
+   /*Usuarios.findAll({where: {email: user}}).then(function(usuarios){
+       res.send({Usuarios: usuarios})
+   })
+   res.send(user);
+})*/
 
-app.listen(443, () => {
+
+
+app.listen(8080, () => {
     console.log("Servidor rodando na porta 8080");
 })
